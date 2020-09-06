dde55aa105a0b12ff4728e83950c48a8f1802ecc
isbananas <58787358+isbananas@users.noreply.github.com>
2020-09-06 19:19:02 -0400

    Update and Rename README.md to caesar_cipher.rb

---
+++ caesar_cipher.rb
@@ -0,0 +1,19 @@
+
+def caesar_cipher(str,shift=1)
+alphabet = ("a".."z").to_a
+caesar = ""
+
+str.each_char do |letter|
+  if letter == " "
+    caesar += " "
+  else
+    old_index = alphabet.find_index(letter)
+    new_index = (old_index + shift) % alphabet.count 
+    caesar += alphabet[new_index]
+  end
+end
+caesar
+
+end
+
+p caesar_cipher("abc abc", 4)
